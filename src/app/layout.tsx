1379759a8d1266c9741b04bba1ddd6a8fe15020f
Nasef <nassef.ferjeni@gmail.com>
2024-11-11 00:03:22 +0100

    trying stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
       <ReactQueryClientProvider>
         <html lang="en">
           <body
-            // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen grid grid-rows-[auto_1fr_auto]`}
           >
             <Header />
